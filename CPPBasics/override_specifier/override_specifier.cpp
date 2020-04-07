7e07ca2682c549354e76c7e973440eddf950aab4
Dmitry Sigaev <dsigaev@yandex.ru>
2020-04-07 21:09:48 +0300

    >------ Build started: Project: override_specifier, Configuration: Debug Win32 ------ 1>override_specifier.cpp override_specifier.cpp(14,22): error C3668:  'B::getName1': method with override specifier 'override' did not override any base class methods override_specifier.cpp(15,22): error C3668:  'B::getName2': method with override specifier 'override' did not override any base class methods 1>Done building project "override_specifier.vcxproj" -- FAILED. ========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========

--- CPPBasics/override_specifier/override_specifier.cpp
+++ CPPBasics/override_specifier/override_specifier.cpp
@@ -5,21 +5,19 @@ class A
 public:
 	virtual const char* getName1(int x) { return "A"; }
 	virtual const char* getName2(int x) { return "A"; }
+	virtual const char* getName3(int x) { return "A"; }
 };
 
 class B : public A
 {
 public:
-	virtual const char* getName1(short int x) { return "B"; } // тип параметра short int
-	virtual const char* getName2(int x) const { return "B"; } // метод €вл€етс€ const
+	virtual const char* getName1(short int x) override { return "B"; } // ошибка компил€ции, метод не €вл€етс€ переопределением
+	virtual const char* getName2(int x) const override { return "B"; } // ошибка компил€ции, метод не €вл€етс€ переопределением
+	virtual const char* getName3(int x) override { return "B"; } // всЄ хорошо, метод €вл€етс€ переопределением A::getName3(int)
+
 };
 
 int main()
 {
-	B b;
-	A& rParent = b;
-	std::cout << rParent.getName1(1) << '\n';
-	std::cout << rParent.getName2(2) << '\n';
-
 	return 0;
 }
