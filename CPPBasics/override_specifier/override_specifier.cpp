4cdc18f0f42e703a0787f83cdad4d034e3545129
Dmitry Sigaev <dsigaev@yandex.ru>
2020-04-09 10:50:39 +0300

    class 'C' cannot inherit from 'B' as it has been declared as 'final' 1>------ Build started: Project: override_specifier, Configuration: Debug Win32 ------ 1>override_specifier.cpp 1>override_specifier.cpp(16,1): error C3246: 1>override_specifier.cpp(9): message :  see declaration of 'B' 1>Done building project "override_specifier.vcxproj" -- FAILED. ========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========

--- CPPBasics/override_specifier/override_specifier.cpp
+++ CPPBasics/override_specifier/override_specifier.cpp
@@ -6,17 +6,16 @@ public:
 	virtual const char* getName() { return "A"; }
 };
 
-class B : public A
+class B final : public A // обратите внимание на модификатор final здесь
 {
 public:
-	// «аметили final в конце? Ёто означает, что метод переопределить уже нельз€
-	virtual const char* getName() override final { return "B"; } // всЄ хорошо, переопределение A::getName()
+	virtual const char* getName() override { return "B"; }
 };
 
-class C : public B
+class C : public B // ошибка компил€ции: нельз€ наследовать класс final
 {
 public:
-	virtual const char* getName() override { return "C"; } // ошибка компил€ции: переопределение метода B::getName(), который €вл€етс€ final
+	virtual const char* getName() override { return "C"; }
 };
 
 int main()
